383237aa627a44fad8ee15b786a919b28cbb5005
GnitLores <prefuse@gmail.com>
2022-08-05 21:40:53 +0200

    reverse linked list solved

---
+++ Linked List/Reverse Linked List.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Iterative solution. Loop through and link to previous node.
+# O(n) time, faster than 72.1 of solutions.
+class Solution(object):
+    def reverseList(self, head):
+
+        previousNode = None
+        node = head
+
+        while node is not None:
+            nextNode = node.next
+            node.next = previousNode
+            previousNode = node
+            node = nextNode
+
+        return previousNode
+
+# Create list nodes for problem:
+def createNodes(values):
+    if len(values) > 1:
+        nextNode = createNodes(values[1:])
+    else:
+        nextNode = None
+    node = ListNode(values[0], nextNode)
+    return node
+
+def testSolution(*args):
+    sol = Solution
+    result = sol.reverseList(sol, *args)
+    while True:
+        val = result.val
+        print("val: " + str(val))
+        result = result.next
+        if result is None:
+            break
+
+testSolution(createNodes([1,2,3,4,5]))
