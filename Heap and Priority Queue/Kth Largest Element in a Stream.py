477532b5a6bc3d6f71b90c40b4d959f90d79c665
GnitLores <prefuse@gmail.com>
2022-08-08 09:01:28 +0200

    kth largest element in stream solved

---
+++ Heap and Priority Queue/Kth Largest Element in a Stream.py
@@ -0,0 +1,28 @@
+import heapq
+
+# Solution using minimum heap.
+# Smaller values are stored first in array.
+# If we constantly pop elements to keep the array at k size, the first element in the list must be the kth largest value.
+# Building the heap is O(n) time.
+# Adding elements is O(log n) time.
+# Reading the kth largest element is O(1) time.
+class KthLargest(object):
+    def __init__(self, k, nums):
+        self.k, self.minheap = k, nums
+        heapq.heapify(self.minheap)
+        while len(self.minheap) > k:
+            heapq.heappop(self.minheap)
+
+    def add(self, val):
+        heapq.heappush(self.minheap, val)
+        if len(self.minheap) > self.k:
+            heapq.heappop(self.minheap)
+        return self.minheap[0]
+
+def testSolution(k, nums, additional):
+    obj = KthLargest(k, nums)
+    for n in additional:
+        print(str(n) + " added: " + str(obj.add(n)))
+
+testSolution(3, [4, 5, 8, 2], [3, 5, 10, 9, 4])
+
