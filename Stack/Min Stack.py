c03f7c28d7c10f56459c41ed61f4384b0b33e0e5
GnitLores <prefuse@gmail.com>
2022-08-12 08:33:26 +0200

    comments

--- Stack/Min Stack.py
+++ Stack/Min Stack.py
@@ -1,5 +1,8 @@
 class MinStack:
-
+    # With a stack, we know the order in which values will be removed.
+    # Because of this, we can keep track of the minimum value by using a separate stack.
+    # When pushing a value to the main stack, also push the current minimum value to separate stack.
+    # All operations are O(1).
     def __init__(self):
         self.stack = []
         self.minVals = []
