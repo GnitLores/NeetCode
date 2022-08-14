97c9b6eb83a572b5c3a1e6095cebbd5d14c35611
GnitLores <prefuse@gmail.com>
2022-08-14 18:58:09 +0200

    more efficient forward iterating solution

--- Greedy/Jump Game II.py
+++ Greedy/Jump Game II.py
@@ -1,22 +1,29 @@
-from typing import List
-import math
+class Solution(object):
+    # Iterate through and for each position:
+    # Find the jump that enables the greatest distance on the subsequent jump.
+    def jump(self, nums):
+        if len(nums) == 1:
+            return 0
 
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        nums.reverse()
-        jumpsNeeded = [0]*len(nums)
-        for i in range(1, len(nums)):
-            jumpLength = nums[i]
-            if jumpLength == 0:
-                jumpsNeeded[i] = math.inf
-                continue
-            maxLanding = max(i - jumpLength, 0)
-            possibleJumps = jumpsNeeded[maxLanding:i]
-            bestJump = min(possibleJumps)
-            jumpsNeeded[i] = bestJump + 1
-        return jumpsNeeded[-1]
+        end = len(nums) - 1
+        jumps = 0
+        ptr = 0
+        while ptr < end:
+            jumps += 1
+
+            bestJump = ptr
+            jumpPtr = ptr
+            while jumpPtr < min(ptr + nums[ptr], end): # Search all possible jumps from this position or to end of array if closer
+                jumpPtr += 1
+
+                if jumpPtr == end: # If end is reachable from this position
+                    return jumps
+
+                if jumpPtr + nums[jumpPtr] > bestJump + nums[bestJump]: # If this gives the best subsequent jump so far
+                    bestJump = jumpPtr
+            ptr = bestJump
 
 sol = Solution()
-# print(sol.jump([3,3,3,3,4]))
+print(sol.jump([3,3,3,3,4]))
 print(sol.jump([2,3,0,1,4]))
-# print(sol.canJump([3,2,1,0,4]))
+print(sol.jump([2,3,2,0,1,1,1,1,4]))
