0dac304571f3966ffd9520e1276f6c00b28d4433
GnitLores <prefuse@gmail.com>
2022-08-15 11:27:00 +0200

    rotate image solved

--- Math and Geometry/Rotate Image.py
+++ Math and Geometry/Rotate Image.py
@@ -1,17 +1,18 @@
 class Solution(object):
+    # Rotate in layers.
+    # O(n^2)
     def rotate(self, matrix):
+        print("")
         for row in matrix:
             print(row)
 
-        rows = len(matrix)
-        cols = len(matrix[0])
-
-        r = 0
-        for c in range(cols-1):
-            val = matrix[r][c]
-            for _ in range(4):
-                val, matrix[c][-1-r] = matrix[c][-1-r], val
-                r, c = c, -1-r
+        for layer in range(0, len(matrix)//2):  # There are half as many layers as there are rows/columns (rounding down if odd number to exlude center element which remains constant)
+            r = layer
+            for c in range(layer, len(matrix[0]) -1 -layer): # For each element in the top row of layer (exluding last element which will be replaced by first on rotation):
+                val = matrix[r][c] # Store the value
+                for _ in range(4): # In four steps:
+                    val, matrix[c][-1-r] = matrix[c][-1-r], val # Insert value rotated 90 degrees clockwise and store the value it replaces
+                    r, c = c, -1-r # rotate coordinates 90 degrees clockwise
 
         print("")
         for row in matrix:
@@ -22,3 +23,9 @@ sol.rotate([
     [1,2,3],
     [4,5,6],
     [7,8,9]])
+
+sol.rotate([
+    [5,1,9,11],
+    [2,4,8,10],
+    [13,3,6,7],
+    [15,14,12,16]])
