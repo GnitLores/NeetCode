a906a9a0d7ceeda438c0968aa4ac6b7def45d4b8
GnitLores <prefuse@gmail.com>
2022-08-16 11:58:47 +0200

    comments

--- Stack/Daily Temperatures.py
+++ Stack/Daily Temperatures.py
@@ -1,6 +1,6 @@
 class Solution(object):
-    # The key is to realize that for a day, if a later day has a higher temperature,
-    # then it also has a higher temperature than all days in between.
+    # The key is to realize, thatfor a day we have not yet found a hotter day for,
+    # when we do find a hotter day, it will also be hotter than all days in between.
     # This means that we can iterate through, storing the temperature and index of day on a stack.
     # Then we compare subsequent days to the top of the stack, and calculate the difference in days.
     # Keep popping days from the stack until the top is not colder than the current day.
