2f5bf7741a73921c5a562c1e4d06e7a4370d1931
GnitLores <prefuse@gmail.com>
2022-08-16 11:18:30 +0200

    daily temperatures solved

---
+++ Stack/Daily Temperatures.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    # The key is to realize that for a day, if a later day has a higher temperature,
+    # then it also has a higher temperature than all days in between.
+    # This means that we can iterate through, storing the temperature and index of day on a stack.
+    # Then we compare subsequent days to the top of the stack, and calculate the difference in days.
+    # Keep popping days from the stack until the top is not colder than the current day.
+    # O(n) time.
+    def dailyTemperatures(self, temperatures):
+        res = [0] * len(temperatures)
+        stack = []
+        idx = []
+        stack.append(temperatures[0])
+        idx.append(0)
+        for i in range(1, len(temperatures)):
+
+            while len(stack) > 0 and temperatures[i] > stack[-1]:
+                stack.pop()
+                old = idx.pop()
+                res[old] = i - old
+                
+            stack.append(temperatures[i])
+            idx.append(i)
+        return res
+
+sol = Solution()
+print(sol.dailyTemperatures([73,74,75,71,69,72,76,73]))
