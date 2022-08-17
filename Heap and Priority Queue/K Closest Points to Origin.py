870e55d9337bc597d5e8bec35b6455aee40cf470
GnitLores <prefuse@gmail.com>
2022-08-17 18:12:04 +0200

    clean up

--- Heap and Priority Queue/K Closest Points to Origin.py
+++ Heap and Priority Queue/K Closest Points to Origin.py
@@ -1,7 +1,9 @@
-from cmath import sqrt
 from typing import List
 import heapq
 
+# Minheap solution, heapifying points by distance to origin.
+# Calculation optimized by omitting square root.
+# O(n) to heapify and O(klogn) to retrieve. Very efficient if k is small.
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         dist = [(x**2 + y**2) for x, y in points] # Real calculation is (x**2 + y**2)**0.5, but taking the square root to find the true distance will not change the ordering.
