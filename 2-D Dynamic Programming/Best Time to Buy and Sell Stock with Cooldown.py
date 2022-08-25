696e337489b5ea9ad9bb65bbde46cdd03771b21f
GnitLores <prefuse@gmail.com>
2022-08-25 11:12:52 +0200

    comments

--- 2-D Dynamic Programming/Best Time to Buy and Sell Stock with Cooldown.py
+++ 2-D Dynamic Programming/Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,7 +1,20 @@
 from typing import List
 
-
 class Solution:
+    # Dynamic programming solution. Recursive top down solution with caching.
+    #
+    # We can always be in one of two states (in addition to being forced to wait after selling).
+    # If we have not bought, then we can only buy or wait.
+    # If we have bought, then we can only either sell or wait.
+    # Thus there is only at most two decisions per branch in the decision tree, and the recursion relation is:
+    # Can buy - the max of either (buying today + max profit being in selling state next day) and (waiting and being in buying state next day)
+    # Can sell - the max of either (selling today + max profit of being in buying state in two days) and (waiting and being in buysing state next day)
+    # An extra cooldown day must pass after selling.
+    # We can do a dfs through the decision tree and cache the results for each day with a hashset using a tuple key (i, canBuy).
+    # There are at most 2 * n possible distinct subproblem results, so with caching this is linear time.
+    #
+    # O(n) time.
+
     def maxProfit(self, prices: List[int]) -> int:
         cache = dict()
 
