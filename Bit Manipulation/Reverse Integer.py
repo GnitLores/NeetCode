d9dafc2a1f2b096a92c25df4ed62e28839aa7c07
GnitLores <prefuse@gmail.com>
2022-08-26 10:26:43 +0200

    reverse integer solved

--- Bit Manipulation/Reverse Integer.py
+++ Bit Manipulation/Reverse Integer.py
@@ -1,6 +1,14 @@
 import math
 
 class Solution:
+    # Solving this in python is tricky because arbitrary size integers are allowed,
+    # which means that there is no overflow to detect, and because mod and integer division
+    # with negative numbers do not work as in c/c++, so we have to do floating point and
+    # cast to int. Python rounds toward -inf while c/c++ rounds towards zero.
+    #
+    # We can check for 32 bit overflow by checking if the intermediate result exceeds
+    # the limits without the last digit, or if it is equal to it, if the digit to append
+    # exceeds the last digit of the limit.
     def reverse(self, x: int) -> int:
         MIN = -2147483648  # -2^31,
         MAX = 2147483647  #  2^31 - 1
