9093743573316d004cf6c8a3a1a65187204ec633
GnitLores <prefuse@gmail.com>
2022-08-30 07:00:13 +0200

    comments

--- Trie/Word Search II.py
+++ Trie/Word Search II.py
@@ -1,6 +1,7 @@
 from typing import List
 import collections
 
+
 class TrieNode:
     def __init__(self):
         self.children = {}
@@ -13,7 +14,8 @@ class TrieNode:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
         cur.isWord = True
-        
+    
+    # Used to prune a word once it has been found for better performance.
     def pruneWord(self, word) -> None:
         cur: TrieNode = self
         nodeAndChildKey: list[tuple[TrieNode, str]] = []
@@ -30,6 +32,15 @@ class TrieNode:
 
         
 class Solution:
+    # Neetcode solution.
+    # Similar dfs approach to naive solution below, but also making use of a trie.
+    # All target words are added to trie so they can be searched for simultaneously.
+    # This way, common prefixes can be searched for once, and only once the words diverge
+    # does the dfs diverge as well.
+    # A dfs is started from each tile. Continue as long as the dfs mactches a word in the trie.
+    # However, this is no longer enough to pass on leetcode.
+    # More difficult test cases have been added.
+    # Now it is also necessary to prune words that have been found, which is a more complicated operation.
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = TrieNode()
         for w in words:
@@ -64,7 +75,13 @@ class Solution:
         
         return res
 
-
+    # First attempt, naive solution.
+    # Works well, but isn't fast enough to pass on leetcode.
+    # The are test cases with a large amount of very similar target words.
+    # We need to use a trie to process these efficiently.
+    # Make dictionary mapping each alphabet letter to tiles.
+    # Make array of dictionaries mapping tiles to connected letters.
+    # Do a dfs for each valid start position for each target word.
     def findWordsNaive(self, board: List[List[str]], words: List[str]) -> List[str]:
         rows = len(board)
         cols = len(board[0])
