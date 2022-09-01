dc7dc282ed3a91b4ad29eedb602c57ec2d8a74cf
GnitLores <prefuse@gmail.com>
2022-09-01 08:15:47 +0200

    comments

--- 2-D Dynamic Programming/Distinct Subsequences.py
+++ 2-D Dynamic Programming/Distinct Subsequences.py
@@ -1,5 +1,21 @@
 class Solution:
-    
+    # 2-D dynamic programming solution.
+    # Suppose that we add a letter to the string we are searching in.
+    # If that letter does not match the last letter of the string we are searching for,
+    # it does not change the number of possible substrings.
+    # If it does match, then the number of substrings is the number we had before adding it,
+    # plus the number of substrings when excluding the last letter of both strings.
+    # This is because the new last letter becomes a valid final letter of all of those substrings.
+    # Thus the recursion relation is:
+    # substrings(s, t):
+    #   if s[-1] == t[-1]:
+    #       substrings = substrings(s_(n-1), t) + substrings(s_(n-1), t_(n-1))
+    #   else:
+    #       substrings = substrings(s_(n-1), t)
+    #
+    # This solution uses a 2D array to calculate this iteratively.
+    # The entire left column is ones because there is one way to find an empty string in any string (no letters).
+    # O(s * t) time.
     def numDistinct(self, s: str, t: str) -> int:
         dp = [[0 for _ in range(len(t) + 1)] for _ in range(len(s) + 1)]
         for r in range(len(s) + 1):
