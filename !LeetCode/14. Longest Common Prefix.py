8a0f7ce48d9117e1448273117062c0b66912f3bf
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-02 10:24:28 +0200

    comments

--- !LeetCode/14. Longest Common Prefix.py
+++ !LeetCode/14. Longest Common Prefix.py
@@ -2,6 +2,10 @@ from typing import List
 
 
 class Solution:
+    # Use zip on the unpacked list of strings gives a list of tuples
+    # with each tuple containing the ith char of that string.
+    # Check if all chars are the same.
+    # Zip stops with the shortest input, so we don't have to check for that.
     def longestCommonPrefix(self, strs: List[str]) -> str:
         res = []
         for group in zip(*strs):
