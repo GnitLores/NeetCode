782590b50eee9be2cc25aa1a214937a4c5e9e06d
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-07 08:33:49 +0200

    slow/fast pointer solution

--- !LeetCode/234. Palindrome Linked List.py
+++ !LeetCode/234. Palindrome Linked List.py
@@ -5,6 +5,56 @@ class ListNode:
         self.next = next
 
 class Solution:
+    # It is possible to do this in place
+    # using only constant extra space.
+    # The code is quite complicated, but it is very efficient.
+    # - Use slow and fast pointer technique to locate first node
+    # of seconf half.
+    # - From there, use slow pointer to reverse links in second half.
+    # - Run through from both ends and check if palindrome.
+
+    def isPalindrome(self, head: ListNode) -> bool:
+
+        # Detect midpoint.
+        # Slow pointer will end at first node of second half.
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # If list is too short to need reversing.
+        if slow.next == None:
+            if head.val == slow.val:
+                return True
+            else: 
+                return False
+
+        # Reverse the links of the second half with the slow pointer.
+        prev = slow
+        slow = slow.next
+        prev.next = None
+        while slow:
+            next = slow.next
+            slow.next = prev
+            prev = slow
+            if next:
+                slow = next
+            else:
+                break
+        
+        # Check if palindrome.
+        # Check if values are the same and if the pointers reach the same node.
+        # Check for same node after moving each pointer, because if the list is
+        # even, they will end up next to each other and could otherwise skip past each other.
+        while True:
+            if head.val == slow.val:
+                if head == slow: return True
+                head = head.next
+                if head == slow: return True
+                slow = slow.next
+            else:
+                return False
+
     # Naive solution.
     # Just make an array, read all the values into it, 
     # and check if it is a palindrom with two pointers.
@@ -26,6 +76,9 @@ class Solution:
         return True
 
 sol = Solution()
+print(sol.isPalindrome(ListNode(1, ListNode(2, ListNode(3, ListNode(2, ListNode(1)))))))
+print(sol.isPalindrome(ListNode(1, ListNode(2, ListNode(2, ListNode(1))))))
+print(sol.isPalindrome(ListNode(1, ListNode(2))))
 print("")
 print(sol.isPalindromeNaive(ListNode(1, ListNode(2, ListNode(3, ListNode(2, ListNode(1)))))))
 print(sol.isPalindromeNaive(ListNode(1, ListNode(2, ListNode(2, ListNode(1))))))
