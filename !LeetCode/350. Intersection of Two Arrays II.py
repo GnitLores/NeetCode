18b16fa917c5fb7da053a9ec0500952408d960a0
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-09 09:20:57 +0200

    single dictionary solution

--- !LeetCode/350. Intersection of Two Arrays II.py
+++ !LeetCode/350. Intersection of Two Arrays II.py
@@ -3,8 +3,23 @@ from typing import List
 
 
 class Solution:
+    # Solution using dictionary.
+    # Count numbers in one array, and iterate through the other, adding shared
+    # numbers and decrementing count.
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count1 = collections.defaultdict(int)
+        for n in nums1:
+            count1[n] += 1
+        res = []
+        for n in nums2:
+            if count1[n] > 0:
+                res.append(n)
+                count1[n] -= 1
+        return res
+
     # Solution using two dictionaries.
     # Count both arrays and add minimum count to output.
+    # A bit less efficient.
     def intersectTwoDicts(self, nums1: List[int], nums2: List[int]) -> List[int]:
         count1 = collections.defaultdict(int)
         count2 = collections.defaultdict(int)
@@ -20,5 +35,7 @@ class Solution:
         return res
 
 sol = Solution()
+print(sol.intersect(nums1 = [1,2,2,1], nums2 = [2,2]))
+print(sol.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4]))
 print(sol.intersectTwoDicts(nums1 = [1,2,2,1], nums2 = [2,2]))
 print(sol.intersectTwoDicts(nums1 = [4,9,5], nums2 = [9,4,9,8,4]))
