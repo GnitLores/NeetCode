743dc6b96b49bcbdd742d5e40104a57f91d7fe72
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-10 10:22:28 +0200

    string based solution

--- !LeetCode/9. Palindrome Number.py
+++ !LeetCode/9. Palindrome Number.py
@@ -1,4 +1,18 @@
 class Solution:
+    # Cast to string and check with two pointers.
+    def isPalindromeString(self, x: int) -> bool:
+        if x < 0: return False
+        if x < 10: return True
+
+        x = str(x)
+        l, r = 0, len(x) - 1
+        while l < r:
+            if x[l] != x[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
     # Strip least significant digit and add to list.
     # Check list with two pointers.
     def isPalindromeDigits(self, x: int) -> bool:
@@ -19,6 +33,10 @@ class Solution:
         return True
 
 sol = Solution()
+print(sol.isPalindromeString(121))
+print(sol.isPalindromeString(-121))
+print(sol.isPalindromeString(10))
+print("")
 print(sol.isPalindromeDigits(121))
 print(sol.isPalindromeDigits(-121))
 print(sol.isPalindromeDigits(10))
