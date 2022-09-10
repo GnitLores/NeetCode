209ef39e813aece2b627e2d4c990639448130f52
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-10 10:23:09 +0200

    more efficient solutions

--- !LeetCode/9. Palindrome Number.py
+++ !LeetCode/9. Palindrome Number.py
@@ -1,4 +1,29 @@
 class Solution:
+    # Solve by reversing the number.
+    # Find the least significant digit with mod 10, and
+    # strip it with integer division by 10.
+    # Add it to the reverse number and multiply by 10 for every
+    # digit to get the least significant digit at the most significant
+    # place.
+    def isPalindromeReverseDigits(self, x: int) -> bool:
+        if x < 0: return False
+        if x < 10: return True
+
+        originalNum = x
+        reverseNum = 0
+        while x > 0:
+            reverseNum *= 10
+            reverseNum += x % 10
+            x //= 10
+        return originalNum == reverseNum
+    
+    # Just cast to string, reverse and check.
+    def isPalindromeReverseString(self, x: int) -> bool:
+        if x < 0: return False
+        if x < 10: return True
+
+        return str(x) == str(x)[::-1]
+
     # Cast to string and check with two pointers.
     def isPalindromeString(self, x: int) -> bool:
         if x < 0: return False
@@ -33,6 +58,14 @@ class Solution:
         return True
 
 sol = Solution()
+print(sol.isPalindromeReverseDigits(121))
+print(sol.isPalindromeReverseDigits(-121))
+print(sol.isPalindromeReverseDigits(10))
+print("")
+print(sol.isPalindromeReverseString(121))
+print(sol.isPalindromeReverseString(-121))
+print(sol.isPalindromeReverseString(10))
+print("")
 print(sol.isPalindromeString(121))
 print(sol.isPalindromeString(-121))
 print(sol.isPalindromeString(10))
