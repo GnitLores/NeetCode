aa3651efb668c52ea9a62a3fe0d30f14029c306e
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-11 08:01:27 +0200

    length of last word

---
+++ !LeetCode/58. Length of Last Word.py
@@ -0,0 +1,21 @@
+class Solution:
+    # Reverse string, strip leading whitespace.
+    # Index of first space corresponds to length of first word in reversed string.
+    def lengthOfLastWord(self, s: str) -> int:
+        if not s: return 0
+        s = s[::-1].strip()
+        if not s: return 0
+
+        i = 0
+        while i < len(s):
+            if s[i] == " ":
+                break
+            i += 1
+        return i
+
+sol = Solution()
+print(sol.lengthOfLastWord(s = ""))
+print(sol.lengthOfLastWord(s = "    "))
+print(sol.lengthOfLastWord(s = "Hello World"))
+print(sol.lengthOfLastWord(s = "   fly me   to   the moon  "))
+print(sol.lengthOfLastWord(s = "luffy is still joyboy"))
