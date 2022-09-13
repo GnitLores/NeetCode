50712888a2825eebcdfc0e8a383dec20f833d692
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-13 08:15:08 +0200

    inner loop solution

--- !LeetCode/83. Remove Duplicates from Sorted List.py
+++ !LeetCode/83. Remove Duplicates from Sorted List.py
@@ -4,6 +4,17 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
+    # Slightly simpler and more efficient solution.
+    # No need to keep track of previous node, just jump
+    # over equal value nodes in an inner loop.
+    def deleteDuplicatesDoubleLoop(self, head: ListNode) -> ListNode:
+        node = head
+        while node:
+            while node.next and node.val == node.next.val:
+                node.next = node.next.next
+            node = node.next
+        return head
+
     # Keep track of previous node. If current node is equal to previous,
     # point from previous node directly to next relative to current node.
     def deleteDuplicates(self, head: ListNode) -> ListNode:
@@ -19,6 +30,12 @@ class Solution:
         return head
 
 sol = Solution()
+res = sol.deleteDuplicatesDoubleLoop(ListNode(1, ListNode(1, ListNode(1))))
+print("")
+res = sol.deleteDuplicatesDoubleLoop(ListNode(1, ListNode(1, ListNode(2))))
+print("")
+res = sol.deleteDuplicatesDoubleLoop(ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(3))))))
+print("")
 res = sol.deleteDuplicates(ListNode(1, ListNode(1, ListNode(1))))
 print("")
 res = sol.deleteDuplicates(ListNode(1, ListNode(1, ListNode(2))))
