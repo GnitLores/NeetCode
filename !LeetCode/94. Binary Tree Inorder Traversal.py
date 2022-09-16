bc9651eef00b99128077144fa2c741433945967b
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-16 10:37:49 +0200

    compact

--- !LeetCode/94. Binary Tree Inorder Traversal.py
+++ !LeetCode/94. Binary Tree Inorder Traversal.py
@@ -1,28 +1,21 @@
 from typing import List
 
-
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
 class Solution:
     # Inorder traversal evaluates the left subtree first, then the root, then the right subtree.
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         if not root: return []
         res = []
         def inOrderDfs(root: TreeNode):
-            if root.left:
-                inOrderDfs(root.left)
-            
+            if root.left: inOrderDfs(root.left)
             res.append(root.val)
-
-            if root.right:
-                inOrderDfs(root.right)
-
+            if root.right: inOrderDfs(root.right)
         inOrderDfs(root)
-
         return res
 
 sol = Solution()
