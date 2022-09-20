761f3ad2869b810f357995fa7340830f09f70fd5
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-20 09:15:03 +0200

    solution based on last occurrence only

--- !LeetCode/219. Contains Duplicate II.py
+++ !LeetCode/219. Contains Duplicate II.py
@@ -2,6 +2,21 @@ from typing import List
 import collections
 
 class Solution:
+    # Second attempt:
+    # We actually only need to know the last instance of each number because
+    # it will always be the closest to the next occurrence of that number.
+    # We just make a dictionary mapping numbers to the LAST OCCURRENCE of that
+    # number encountered.
+    # More efficient and simple.
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = collections.defaultdict(int)
+        for i, n in enumerate(nums):
+            if n in hashmap and i - hashmap[n] <= k:
+                return True
+            hashmap[n] = i
+        return False
+
+    # First attempt:
     # Make dictionary mapping numbers to set of indices with those numbers.
     # For each number, check if it is already mapped, and if one of those indices
     # is closer than k.
@@ -22,6 +37,10 @@ class Solution:
         return False
 
 sol = Solution()
+print(sol.containsNearbyDuplicate(nums = [1,2,3,1], k = 3))
+print(sol.containsNearbyDuplicate(nums = [1,0,1,1], k = 1))
+print(sol.containsNearbyDuplicate(nums = [1,2,3,1,2,3], k = 2))
+print("")
 print(sol.containsNearbyDuplicateSet(nums = [1,2,3,1], k = 3))
 print(sol.containsNearbyDuplicateSet(nums = [1,0,1,1], k = 1))
 print(sol.containsNearbyDuplicateSet(nums = [1,2,3,1,2,3], k = 2))
