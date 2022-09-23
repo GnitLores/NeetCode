cf9913e98915fc39c9e4628aef1b0a2b9453a8bd
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-23 06:46:28 +0200

    amortized constant time solution

--- !LeetCode/232. Implement Queue using Stacks.py
+++ !LeetCode/232. Implement Queue using Stacks.py
@@ -1,4 +1,29 @@
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack2.append(x)
+
+    def pop(self) -> int:
+        self.queuify()
+        return self.stack1.pop()
+
+    def peek(self) -> int:
+        self.queuify()
+        return self.stack1[-1]
+    
+    def queuify(self) -> None: 
+        if not self.stack1:
+            while self.stack2:
+                self.stack1.append(self.stack2.pop())
+
+    def empty(self) -> bool:
+        return (len(self.stack1) + len(self.stack2)) == 0
+
 class MyQueueSimple:
+
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
@@ -18,6 +43,16 @@ class MyQueueSimple:
 
     def empty(self) -> bool:
         return len(self.stack1) == 0
+
+obj = MyQueue()
+print(obj.empty())
+obj.push(1)
+obj.push(2)
+obj.push(3)
+obj.push(4)
+print(obj.pop())
+print(obj.peek())
+print(obj.empty())
 
 print("")
 obj = MyQueueSimple()
