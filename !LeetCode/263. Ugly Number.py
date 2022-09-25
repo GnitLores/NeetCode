435ae1c774a7b5f874872c826e0e2aa79354323d
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-25 10:24:39 +0200

    comments

--- !LeetCode/263. Ugly Number.py
+++ !LeetCode/263. Ugly Number.py
@@ -1,4 +1,12 @@
 class Solution:
+    # The factors of a negative integer cannot possibly be limited to 2, 3, and 5, so always false.
+    # 0 has an infinite number of factors so always false.
+    # 1 has no prime factors other than 2, 3, and 5 (and not those either) so always true.
+    # Prime factorization is unique, so there exists a unique combination of primes that multiply to any positive integer.
+    # We can just repeatedly check if the number is divisible by 2, 3 and 5 and divide if it is.
+    # If we come to a number that is not, then it is not an ugly number, and multiplying it by 2, 3 or 5 won't make it one.
+    # Thus the original number was not ugly.
+    # If it was ugly we would inevitably arrive at 1.
     def isUgly(self, n: int) -> bool:
         if n == 1: return True
         if n < 1: return False
