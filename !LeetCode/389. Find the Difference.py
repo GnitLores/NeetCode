006a3219f280dedaefc4ed039a501d09c991ffc2
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-28 10:01:29 +0200

    bit manipulation solution

--- !LeetCode/389. Find the Difference.py
+++ !LeetCode/389. Find the Difference.py
@@ -2,6 +2,20 @@ import collections
 
 
 class Solution:
+    # Solution using bit manipulation.
+    # XORing two identical numbers gives zero.
+    # XORing zero and a number gives the number.
+    # The order in which the numbers are XORed does not matter.
+    # Consequently, XORing all the numbers cancels out all numbers that appear an even number of times.
+    # The result will be the only number that does not have a duplicate.
+    def findTheDifferenceBitManipulation(self, s: str, t: str) -> str:
+        res = 0
+        for c in s:
+            res = res ^ ord(c)
+        for c in t:
+            res = res ^ ord(c)
+        return chr(res)
+
     # Solution using dictionary to count characters.
     def findTheDifference(self, s: str, t: str) -> str:
         cnt = collections.defaultdict(int)
@@ -16,5 +30,8 @@ class Solution:
         return list(cnt.keys())[0]
 
 sol = Solution()
+print(sol.findTheDifferenceBitManipulation(s = "abcd", t = "abcde"))
+print(sol.findTheDifferenceBitManipulation(s = "", t = "y"))
+print("")
 print(sol.findTheDifference(s = "abcd", t = "abcde"))
 print(sol.findTheDifference(s = "", t = "y"))
