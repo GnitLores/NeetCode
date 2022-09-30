3b7797e8daff6abe62dcfd87a91ad7979c299e6c
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-09-30 09:26:31 +0200

    solution using outer scope variable

--- !LeetCode/404. Sum of Left Leaves.py
+++ !LeetCode/404. Sum of Left Leaves.py
@@ -5,7 +5,24 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
+    # Even simpler solution using an outer scope variable
+    # instead of passing back the sum.
+    def sumOfLeftLeavesSimple(self, root: TreeNode) -> int:
+        sum = [0]
+        def dfs(root: TreeNode, isLeft: bool) -> int:
+            if not root: return
+            if isLeft and not root.left and not root.right:
+                sum[0] += root.val
+            dfs(root.left, True)
+            dfs(root.right, False)
+        dfs(root, False)
+        return sum[0]
+
+
     # Depth first search.
+    # Pass through left/right status of node from parent node.
+    # Find sum for subtree if not leaf.
+    # Return value if left leaf or return zero if right leaf.
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
         def dfs(root: TreeNode, isLeft: bool) -> int:
             if not root: return 0
@@ -24,6 +41,10 @@ class Solution:
         return dfs(root, False)
 
 sol = Solution()
+print(sol.sumOfLeftLeavesSimple(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))))
+print(sol.sumOfLeftLeavesSimple(TreeNode(1)))
+print(sol.sumOfLeftLeavesSimple(None))
+print("")
 print(sol.sumOfLeftLeaves(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))))
 print(sol.sumOfLeftLeaves(TreeNode(1)))
 print(sol.sumOfLeftLeaves(None))
