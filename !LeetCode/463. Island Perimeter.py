921f969b587705b78446869e7ed9f20e741e6ac9
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-10-04 08:58:58 +0200

    bfs solution

--- !LeetCode/463. Island Perimeter.py
+++ !LeetCode/463. Island Perimeter.py
@@ -20,6 +20,43 @@ class Solution:
                     if c == cols - 1 or grid[r][c + 1] == 0: res += 1
         return res
 
+    # Breadth first search solution.
+    # Find the first island tile and start a BFS from it.
+    # Check every direction, and if it is outside the array or
+    # not an island tile, add a border.
+    # If it is an unvisited island tile, add it to the queue.
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def findStartPoint():
+            for r in range(rows):
+                for c in range(cols):
+                    if grid[r][c] == 1:
+                        return r, c
+        r, c = findStartPoint()
+
+        queue = deque()
+        visited = set()
+        queue.append((r, c))
+        visited.add((r, c))
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        res = 0
+        while queue:
+            pr, pc = queue.popleft()
+
+            for dr, dc in dirs:
+                r = pr + dr
+                c = pc + dc
+                if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
+                    res += 1
+                else:
+                    if not (r, c) in visited:
+                        queue.append((r, c))
+                        visited.add((r, c))
+        return res
+
 sol = Solution()
 print(sol.islandPerimeterSimple(grid = [
     [0,1,0,0],
@@ -28,3 +65,11 @@ print(sol.islandPerimeterSimple(grid = [
     [1,1,0,0]]))
 print(sol.islandPerimeterSimple(grid = [[1]]))
 print(sol.islandPerimeterSimple(grid = [[1,0]]))
+print("")
+print(sol.islandPerimeter(grid = [
+    [0,1,0,0],
+    [1,1,1,0],
+    [0,1,0,0],
+    [1,1,0,0]]))
+print(sol.islandPerimeter(grid = [[1]]))
+print(sol.islandPerimeter(grid = [[1,0]]))
