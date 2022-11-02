5d4a112959e081e389ecdbfa02029b8c699b6f60
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-11-02 09:40:17 +0100

    linear time solution

--- !LeetCode/796. Rotate String.py
+++ !LeetCode/796. Rotate String.py
@@ -2,6 +2,37 @@ import collections
 
 
 class Solution:
+    # Linear time solution.
+    # Algorithmically more efficient than the other solution
+    # but much more complicated.
+    # Check that length is equal and that char counts are equal.
+    # Use sliding window technique to identify the segment starting
+    # the string s in string goal.
+    # Check if that segment ends string goal, and if the remainder of
+    # string s equals string goal.
+    # If so, s can be rotated to become goal.
+    def rotateStringLinear(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal): return False
+        if collections.Counter(s) != collections.Counter(goal): return False
+
+        maxWin = 0
+        start = 0
+        while start < len(s):
+            while start < len(s) and s[0] != goal[start]:
+                start += 1
+            win = 0
+            while start + win < len(s) and goal[start + win] == s[win]:
+                win += 1
+                if win > maxWin:
+                    winStart = start
+                    maxWin = win
+            win = 0
+            start += 1
+        
+        if winStart + maxWin != len(s): return False
+        if s[maxWin:] == goal[0:len(s) - maxWin]: return True
+        return False
+
     # O(n^2) time slution.
     # Rotating the string as many times as there are
     # characters in it gices the string itself.
@@ -22,6 +53,10 @@ class Solution:
         return False
 
 sol = Solution()
+print(sol.rotateStringLinear(s = "gcmbf", goal = "fgcmb"))
+print(sol.rotateStringLinear(s = "abcde", goal = "cdeab"))
+print(sol.rotateStringLinear(s = "abcde", goal = "abced"))
+print("")
 print(sol.rotateString(s = "gcmbf", goal = "fgcmb"))
 print(sol.rotateString(s = "abcde", goal = "cdeab"))
 print(sol.rotateString(s = "abcde", goal = "abced"))
