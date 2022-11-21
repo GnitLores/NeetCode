462358169ad4beecf813a66e54a64cdb39ce7f92
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-11-21 06:29:59 +0100

    range sum of bst

---
+++ !LeetCode/938. Range Sum of BST.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def buildTree(nums):
+    if not nums: return None
+    numQ = deque(nums)
+    root = TreeNode(numQ.popleft())
+    nodeQ = deque([root])
+    while numQ:
+        node = nodeQ.popleft()
+
+        node.left = TreeNode(numQ.popleft()) if numQ[0] else numQ.popleft()
+        if node.left: nodeQ.append(node.left)
+        if not numQ: break
+
+        node.right = TreeNode(numQ.popleft()) if numQ[0] else numQ.popleft()
+        if node.right: nodeQ.append(node.right)
+
+    return root
+
+class Solution:
+    # Depth first search solution.
+    # Since this is a binary search tree, we can skip searching the left subtree of a node
+    # if it is below the min range since all subtree nodes will have lower values.
+    # The same logic applies to the right subtree of a node that is greater than the max range.
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        def dfs(root: TreeNode):
+            res = root.val if low <= root.val <= high else 0
+            if root.val >= low and root.left: res += dfs(root.left)
+            if root.val <= high and root.right: res += dfs(root.right)
+            return res
+        return dfs(root)
+
+sol = Solution()
+print(sol.rangeSumBST(buildTree([10,5,15,3,7,None,18]), low = 7, high = 15))
+print(sol.rangeSumBST(buildTree([10,5,15,3,7,13,18,1,None,6]), low = 6, high = 10))
