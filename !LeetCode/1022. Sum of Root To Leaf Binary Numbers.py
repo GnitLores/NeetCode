79ec2efbed574f1d3468bb004a854399f069ec3c
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-11-28 06:00:27 +0100

    sum of root to leaf binary numbers

---
+++ !LeetCode/1022. Sum of Root To Leaf Binary Numbers.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # Depth first search solution.
+    # Pass along current value and when finding a leaf, add crrent value to output.
+    # Since higher nodes are more significant, the deeper we go in the tree, the more significant
+    # the higher nodes are.
+    # Multiply the current value by 2 for every level we descend and add the next node.
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        res = []
+        def dfs(root, cur):
+            cur *= 2
+            cur += root.val
+            if not root.left and not root.right:
+                res.append(cur)
+                return
+            if root.left: dfs(root.left, cur)
+            if root.right: dfs(root.right, cur)
+        dfs(root, 0)
+        return sum(res)
