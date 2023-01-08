99a14a4801b2a4e7e4f84c8f02f3296321f08fbb
GnitLores <11030459+GnitLores@users.noreply.github.com>
2023-01-08 14:42:25 +0100

    day of the year

---
+++ !LeetCode/1154. Day of the Year.py
@@ -0,0 +1,21 @@
+from calendar import monthrange
+
+
+class Solution:
+    # In order for a year to qualify as a leap year, it must be divisible by 4. If it is a centurial year, it must also be divisible by 400.
+    def dayOfYear(self, date: str) -> int:
+        days_pr_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        year = int(date[:4])
+        month = int(date[5:7])
+        day = int(date[8:])
+
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+            days_pr_month[1] += 1
+
+        return sum(days_pr_month[: month - 1]) + day
+
+
+sol = Solution()
+print(sol.dayOfYear(date="2019-01-09"))
+print(sol.dayOfYear(date="2019-02-10"))
